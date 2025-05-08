269cdb24cc6b67bd1a631fd546cf4390c1b8acde
Bhui <Bhui@KI840G8-2.ad.nchecr.unsw.edu.au>
2025-05-09 00:03:21 +1000

    Progress in Runnable_ContacMap_Generation_MetaPopulation
    (Initialisation and Connections)

---
+++ src/sim/Runnable_ContacMap_Generation_MetaPopulation.java
@@ -0,0 +1,312 @@
+package sim;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import random.MersenneTwisterRandomGenerator;
+import random.RandomGenerator;
+
+public class Runnable_ContacMap_Generation_MetaPopulation extends Runnable_ClusterModel_ContactMap_Generation_MultiMap {
+
+	public static final int RUNNABLE_FIELD_RMP_CONTACT_MAP_GEN_MULTIMAP_PARTNERSHIP_GRP_MIXING = Runnable_ClusterModel_ContactMap_Generation_MultiMap.LENGTH_RUNNABLE_MAP_GEN_FIELD;
+	public static final int RUNNABLE_FIELD_RMP_CONTACT_MAP_GEN_MULTIMAP_PARTNERSHIP_LOC_CONNECTIONS = RUNNABLE_FIELD_RMP_CONTACT_MAP_GEN_MULTIMAP_PARTNERSHIP_GRP_MIXING
+			+ 1;
+	public static final int LENGTH_RUNNABLE_FIELD_RMP_CONTACT_MAP_GEN = RUNNABLE_FIELD_RMP_CONTACT_MAP_GEN_MULTIMAP_PARTNERSHIP_LOC_CONNECTIONS
+			+ 1;
+
+	public static final String POP_STAT_INITAL_FORMAT = "POP_STAT_INITAL_%d.csv"; // Seed
+	public static final String POP_STAT_FINAL_FORMAT = "POP_STAT_FINAL_%d.csv"; // Seed
+	public static final String POP_STAT_INTERMITTENT_FORMAT = "POP_STAT_INTERMITTENT_%d.csv"; // Seed
+
+	// Population Index from Abstract_Runnable_ClusterModel
+	// POP_INDEX_GRP = 0;
+	// POP_INDEX_ENTER_POP_AGE = POP_INDEX_GRP + 1;
+	// POP_INDEX_ENTER_POP_AT = POP_INDEX_ENTER_POP_AGE + 1;
+	// POP_INDEX_EXIT_POP_AT = POP_INDEX_ENTER_POP_AT + 1;
+	// POP_INDEX_HAS_REG_PARTNER_UNTIL = POP_INDEX_EXIT_POP_AT + 1;
+	private static int POP_INDEX_META_HOME_LOCATION_HIST = Abstract_Runnable_ClusterModel.LENGTH_POP_ENTRIES;
+	private static int LENGTH_POP_INDEX_META = POP_INDEX_META_HOME_LOCATION_HIST + 1;
+
+	protected final int NUM_GENDER;
+	protected final int NUM_LOC;
+	protected final int NUM_AGE_GRP;
+	protected Properties loadedProperties;
+
+	private Pattern pattern_propName = Pattern.compile("RMP_MultMap_(\\d+)_(\\d+)_(\\d+)");
+
+	private HashMap<Integer, int[]> helper_ageRange = null; // K = grp_indec, V = {age_min, age_max}
+	private HashMap<Integer, int[][]> helper_loc_connection = null; // K = location index, V = {connection_1, ...}
+																	// {cumul_weight_1,...}
+
+	public Runnable_ContacMap_Generation_MetaPopulation(long mapSeed, Properties loadedProperties) {
+		super(mapSeed);
+		this.runnable_fields = Arrays.copyOf(this.runnable_fields, LENGTH_RUNNABLE_FIELD_RMP_CONTACT_MAP_GEN);
+		// Load properties
+		this.loadedProperties = loadedProperties;
+		loadRunnableFieldsFromProperties(this.loadedProperties);
+		// Load NUM_GENDER, NUM_LOC and NUM_AGE_GRP
+		Matcher m = pattern_propName.matcher(
+				(CharSequence) loadedProperties.get(SimulationInterface.PROP_NAME[SimulationInterface.PROP_POP_TYPE]));
+		if (m.find()) {
+			NUM_GENDER = Integer.parseInt(m.group(1));
+			NUM_LOC = Integer.parseInt(m.group(2));
+			NUM_AGE_GRP = Integer.parseInt(m.group(3));
+		} else {
+			System.err.printf("Error: Ill-formed popType %s. Simulation will not run.\n ",
+					loadedProperties.get(SimulationInterface.PROP_NAME[SimulationInterface.PROP_POP_TYPE]));
+			NUM_GENDER = 0;
+			NUM_LOC = 0;
+			NUM_AGE_GRP = 0;
+		}
+
+	}
+
+	@Override
+	public void run() {
+		if (NUM_GENDER + NUM_LOC + NUM_AGE_GRP > 0) {
+			HashMap<Integer, Object[]> population = new HashMap<>();
+			int[] contactMapValidRange = (int[]) runnable_fields[RUNNABLE_FIELD_CONTACT_MAP_GEN_VALID_RANGE];
+			final long exportFreq = (long) runnable_fields[RUNNABLE_FILED_EXPORT_FREQ];
+			int[] numInGrp = (int[]) runnable_fields[RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_NUMBER_OF_GRP];
+
+			int popTime = contactMapValidRange[0];
+			int nextId = 1;
+
+			ArrayList<Integer> pop_stat_init_ids = new ArrayList<>();
+			ArrayList<Integer> pop_stat_final_ids = new ArrayList<>();
+
+			HashMap<Integer, ArrayList<Integer>> schedule_move_grp = new HashMap<>(); // K = time, V = list of pids
+			HashMap<Integer, ArrayList<Integer>> active_in_pop = new HashMap<>(); // K = grp, V = list of pids (sorted)
+
+			if (population.isEmpty()) {
+				// Initialise pop
+				for (int g = 0; g < numInGrp.length; g++) {
+					int[] ageRange = getAgeRange(g);
+
+					for (int i = 0; i < numInGrp[g]; i++) {
+						Object[] newPerson = new Object[LENGTH_POP_INDEX_META];
+						newPerson[Abstract_Runnable_ClusterModel.POP_INDEX_GRP] = g;
+						newPerson[Abstract_Runnable_ClusterModel.POP_INDEX_ENTER_POP_AT] = 0;
+						newPerson[Abstract_Runnable_ClusterModel.POP_INDEX_ENTER_POP_AGE] = (int) ageRange[0]
+								+ RNG.nextInt(ageRange[1] - ageRange[2]);
+
+						int exit_grp_time = ageRange[1]
+								- (int) newPerson[Abstract_Runnable_ClusterModel.POP_INDEX_ENTER_POP_AGE];
+						// Note: Exit at in the case is exit the new group
+						newPerson[Abstract_Runnable_ClusterModel.POP_INDEX_EXIT_POP_AT] = exit_grp_time;
+
+						population.put(nextId, newPerson);
+
+						// Schedule exit group
+						ArrayList<Integer> move_grp_pids = schedule_move_grp.get(exit_grp_time);
+						if (move_grp_pids == null) {
+							move_grp_pids = new ArrayList<>();
+							schedule_move_grp.put(exit_grp_time, move_grp_pids);
+						}
+						move_grp_pids.add(nextId);
+
+						// Active by grp
+						ArrayList<Integer> active_by_grp_sorted = active_in_pop.get(g);
+						if (active_by_grp_sorted == null) {
+							active_by_grp_sorted = new ArrayList<>();
+							active_in_pop.put(g, active_by_grp_sorted);
+						}
+						int aI = Collections.binarySearch(active_by_grp_sorted, nextId);
+						if (aI < 0) {
+							active_by_grp_sorted.add(~aI, nextId);
+						}
+						pop_stat_init_ids.add(nextId);
+						nextId++;
+					}
+
+				}
+
+			}
+
+			int numSnapsSim = Math.max(numSnaps, Math.round(contactMapValidRange[1] / snap_dur));
+
+			for (int snapC = 0; snapC < numSnapsSim; snapC++) {
+
+				for (int t = 0; t < snap_dur; t++) {
+
+					ArrayList<Integer> active_by_grp_sorted;
+					ArrayList<Integer> move_grp_pids;
+
+					for (int g = 0; g < numInGrp.length; g++) {
+						int[] gla_index = getGrpIndex(g);
+						active_by_grp_sorted = active_in_pop.get(g);
+						move_grp_pids = schedule_move_grp.remove(popTime);
+						for (int move_grp_pid : move_grp_pids) {
+							Object[] person_stat = population.get(move_grp_pid);
+							int aI = Collections.binarySearch(active_by_grp_sorted, move_grp_pid);
+							if (aI >= 0) {
+								active_by_grp_sorted.remove(aI);
+							}
+							if ((gla_index[2] + 1) < NUM_AGE_GRP) {
+								// Move to new age grp
+								int newGrp = gla_index[2] + 1;
+								person_stat[Abstract_Runnable_ClusterModel.POP_INDEX_GRP] = newGrp;
+								ArrayList<Integer> target_grp_sorted = active_in_pop.get(newGrp);
+								aI = Collections.binarySearch(target_grp_sorted, move_grp_pid);
+								if (aI < 0) {
+									target_grp_sorted.add(~aI, move_grp_pid);
+								}
+
+							} else {
+								// Age out
+								pop_stat_final_ids.add(move_grp_pid);
+							}
+
+						}
+					}
+
+					int[] grpDiff = new int[numInGrp.length];
+
+					for (int g = 0; g < numInGrp.length; g++) {
+						active_by_grp_sorted = active_in_pop.get(g);
+						// Check how many person in grp
+						grpDiff[g] = numInGrp[g] - active_by_grp_sorted.size();
+					}
+
+					// TODO: Attempt to get migration for exceed
+
+					popTime++;
+
+				}
+				// Print intermittent and final pop stat
+				try {
+					PrintWriter pWri;
+					File tarFile;
+					if (!pop_stat_init_ids.isEmpty()) {
+						tarFile = getTargetFile(String.format(POP_STAT_INITAL_FORMAT, mapSeed));
+						boolean append = tarFile.exists();
+						pWri = new PrintWriter(new FileWriter(tarFile, append));
+
+						if (!append) {
+							pWri.println("ID,ENTER_POP_AGE,ENTER_POP_AT_TIME,INIT_GRP");
+						}
+						for (Integer id : pop_stat_init_ids) {
+							Object[] person_stat = population.get(id);
+							pWri.printf("%d,%s,%s,%s\n", id,
+									person_stat[Abstract_Runnable_ClusterModel.POP_INDEX_ENTER_POP_AGE].toString(),
+									person_stat[Abstract_Runnable_ClusterModel.POP_INDEX_ENTER_POP_AT].toString(),
+									person_stat[Abstract_Runnable_ClusterModel.POP_INDEX_GRP].toString());
+
+						}
+						pop_stat_init_ids.clear();
+
+					}
+					if (!pop_stat_final_ids.isEmpty()) {
+						tarFile = getTargetFile(String.format(POP_STAT_FINAL_FORMAT, mapSeed));
+						boolean append = tarFile.exists();
+						pWri = new PrintWriter(new FileWriter(tarFile, append));
+						if (!append) {
+							pWri.println("ID,ENTER_POP_AGE,ENTER_POP_AT_TIME,EXIT_POP_AT");
+						}
+						for (Integer id : pop_stat_final_ids) {
+							Object[] person_stat = population.get(id);
+							pWri.printf("%d,%s,%s,%s\n", id,
+									person_stat[Abstract_Runnable_ClusterModel.POP_INDEX_ENTER_POP_AGE].toString(),
+									person_stat[Abstract_Runnable_ClusterModel.POP_INDEX_ENTER_POP_AT].toString(),
+									person_stat[Abstract_Runnable_ClusterModel.POP_INDEX_EXIT_POP_AT].toString());
+
+						}
+						pop_stat_final_ids.clear();
+					}
+
+				} catch (IOException e) {
+
+					e.printStackTrace(System.err);
+				}
+
+			}
+
+		}
+
+	}
+
+	protected int getGrpIndex(int[] gender_loc_age) {
+		return gender_loc_age[2] + gender_loc_age[1] * NUM_AGE_GRP + gender_loc_age[0] * (NUM_AGE_GRP * NUM_LOC);
+	}
+
+	protected int[] getGrpIndex(int grpNum) {
+		return new int[] { (grpNum / (NUM_AGE_GRP * NUM_LOC)) % NUM_GENDER, (grpNum / NUM_AGE_GRP) % NUM_LOC,
+				grpNum % NUM_AGE_GRP, };
+	}
+
+	/**
+	 * 
+	 * @param location
+	 * @return int[][], with first array as destination and second as weight
+	 */
+	private int[][] getDestinations(int location) {
+		int[][] res;
+		if (helper_loc_connection == null) {
+			helper_loc_connection = new HashMap<>();
+			HashMap<Integer, ArrayList<Integer>> connc_raw = new HashMap<>();
+			int[][] conncs = (int[][]) getRunnable_fields()[RUNNABLE_FIELD_RMP_CONTACT_MAP_GEN_MULTIMAP_PARTNERSHIP_LOC_CONNECTIONS];
+			for (int[] connc : conncs) {
+				int src = connc[0];
+				ArrayList<Integer> arr = connc_raw.get(src);
+				if (arr == null) {
+					arr = new ArrayList<>();
+					connc_raw.put(src, arr);
+				}
+				for (int i = 1; i < connc.length; i++) {
+					arr.add(connc[i]);
+				}
+			}
+			for (Entry<Integer, ArrayList<Integer>> ent : connc_raw.entrySet()) {
+				res = new int[2][ent.getValue().size() / 2];
+				int cumul_weight = 0;
+				int loc_pt = 0;
+
+				for (int i = 0; i < ent.getValue().size(); i += 2) {
+					res[0][loc_pt] = ent.getValue().get(i);
+					res[1][loc_pt] = cumul_weight + ent.getValue().get(i + 1);
+
+					loc_pt++;
+					cumul_weight += res[1][loc_pt];
+				}
+				helper_loc_connection.put(ent.getKey(), res);
+			}
+		}
+
+		res = helper_loc_connection.get(location);
+		return res;
+	}
+
+	private int[] getAgeRange(int grpNum) {
+		if (helper_ageRange == null) {
+			double[][] ageRanges = (double[][]) getRunnable_fields()[RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_AGE_DIST];
+			for (double[] ageRange : ageRanges) {
+				helper_ageRange.put((int) ageRange[0], new int[] { (int) ageRange[1], (int) ageRange[2] });
+			}
+		}
+		int[] res = helper_ageRange.get(grpNum);
+		if (res == null) {
+			res = helper_ageRange.get(~(grpNum % NUM_AGE_GRP));
+			helper_ageRange.put(grpNum, res);
+		}
+		return res;
+	}
+
+	protected int getAge(int pid, int time, HashMap<Integer, Object[]> population) {
+		Object[] person = population.get(pid);
+		return (int) person[Abstract_Runnable_ClusterModel.POP_INDEX_ENTER_POP_AGE]
+				+ (time - (int) person[Abstract_Runnable_ClusterModel.POP_INDEX_ENTER_POP_AT]);
+
+	}
+
+}
