e4bf2aef5d6a469ab4ab0e68d61818663eec883c
Bhui <Bhui@KI840G8-2.ad.nchecr.unsw.edu.au>
2025-09-29 10:13:22 +1000

    Move class files specific to RMP POC to Launcher_RMP_POC

--- src/sim/Simulation_MetaPop.java
+++ src/sim/Simulation_MetaPop.java
@@ -155,13 +155,5 @@ public class Simulation_MetaPop extends Simulation_ClusterModelTransmission {
 		}
 	}
 
-	@Override
-	public Abstract_Runnable_ClusterModel_Transmission generateDefaultRunnable(long cMap_seed, long sim_seed,
-			Properties loadProperties) {
-		Runnable_MetaPopulation_Transmission_RMP_MultiInfection run_trans = new Runnable_MetaPopulation_Transmission_RMP_MultiInfection(
-				cMap_seed, sim_seed, loadedProperties);
-
-		return run_trans;
-	}
-
+	
 }
